076d144321bef66410454e338197fb18cbfb35de
meisydianita <meisymeme13@gmail.com>
2026-03-04 14:38:06 +0700

    Memperbaiki grafik sekum

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -226,23 +226,26 @@
     document.addEventListener('DOMContentLoaded', function() {
       const searchInput = document.getElementById('searchInput');
 
-      // Enter = submit
-      searchInput.addEventListener('keypress', function(e) {
-        if (e.key === 'Enter') {
-          this.form.submit();
-        }
-      });
-
-      // Ketik 2+ huruf = delay 500ms lalu submit (debounce)
-      let timeout;
-      searchInput.addEventListener('input', function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          if (this.value.length >= 0) {
+      // CEK DULU APAKAH ELEMENTNYA ADA
+      if (searchInput) {
+        // Enter = submit
+        searchInput.addEventListener('keypress', function(e) {
+          if (e.key === 'Enter') {
             this.form.submit();
           }
-        }, 50);
-      });
+        });
+
+        // Ketik = delay 50ms lalu submit (debounce)
+        let timeout;
+        searchInput.addEventListener('input', function() {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => {
+            if (this.value.length >= 0) {
+              this.form.submit();
+            }
+          }, 50);
+        });
+      }
     });
   </script>
   <!--end::OverlayScrollbars Configure-->
