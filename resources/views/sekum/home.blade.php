4314bfe0269cfbba33ee20677e080c28c844e158
meisydianita <meisymeme13@gmail.com>
2026-03-04 16:16:11 +0700

    Mengganti kode sekum menjadi bendum

--- resources/views/sekum/home.blade.php
+++ resources/views/sekum/home.blade.php
@@ -16,15 +16,15 @@
         <div class="container-fluid pt-4">
           <!-- Info boxes -->
           <div class="row">
-            <div class="col-12 col-sm-6 col-md-4">
+            <div class="col-12 col-sm-6 col-md-3">
               <div class="info-box">
-                <span class="info-box-icon text-bg-success shadow-sm">
-                  <i class="fa-solid fa-wallet"></i>
+                <span class="info-box-icon text-bg-primary shadow-sm">
+                  <i class="fa-solid fa-envelope"></i>
                 </span>
                 <div class="info-box-content">
-                  <span class="info-box-text">Kas Masuk</span>
+                  <span class="info-box-text">Surat Masuk</span>
                   <span class="info-box-number">
-                    <a href="{{ route('pemasukan.index') }}" class="total-link">Rp {{ number_format($totalkasmasuk, 0, ',', '.') }}</a>
+                    <a href="{{ route('surat-masuk.index') }}" class="total-link">{{ $totalsuratmasuk }}</a>
                   </span>
                 </div>
                 <!-- /.info-box-content -->
@@ -32,15 +32,15 @@
               <!-- /.info-box -->
             </div>
             <!-- /.col -->
-            <div class="col-12 col-sm-6 col-md-4">
+            <div class="col-12 col-sm-6 col-md-3">
               <div class="info-box">
-                <span class="info-box-icon text-bg-primary shadow-sm">
-                  <i class="fa-solid fa-credit-card"></i>
+                <span class="info-box-icon text-bg-danger shadow-sm">
+                  <i class="fa-solid fa-envelope-open"></i>
                 </span>
                 <div class="info-box-content">
-                  <span class="info-box-text">Kas Keluar</span>
+                  <span class="info-box-text">Surat Keluar</span>
                   <span class="info-box-number">
-                    <a href="{{ route('kas-keluar.index') }}" class="total-link">Rp {{ number_format($totalkaskeluar, 0, ',', '.') }}</a>
+                    <a href="{{ route('surat-keluar.index') }}" class="total-link">{{ $totalsuratkeluar }}</a>
                   </span>
                 </div>
                 <!-- /.info-box-content -->
@@ -49,20 +49,75 @@
             </div>
             <!-- /.col -->
 
-            <div class="col-12 col-sm-6 col-md-4">
+            <div class="col-12 col-sm-6 col-md-3">
               <div class="info-box">
-                <span class="info-box-icon text-bg-danger shadow-sm">
-                  <i class="fa-solid fa-money-check"></i>
+                <span class="info-box-icon text-bg-success shadow-sm">
+                  <i class="fa-solid fa-file"></i>
                 </span>
                 <div class="info-box-content">
-                  <span class="info-box-text">Sisa Saldo</span>
+                  <span class="info-box-text">Sertifikat</span>
                   <span class="info-box-number">
-                    <a href="#" class="total-link">Rp {{ number_format($sisasaldo, 0, ',', '.') }}</a>
+                    <a href="{{ route('sertifikat.index') }}" class="total-link">{{ $totalsertifikat }}</a>
                 </div>
                 <!-- /.info-box-content -->
               </div>
               <!-- /.info-box -->
             </div>
+
+            <div class="col-12 col-sm-6 col-md-3">
+              <div class="info-box">
+                <span class="info-box-icon text-bg-secondary shadow-sm">
+                  <i class="fa-solid fa-file-fragment"></i>
+                </span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Dokumen Kegiatan</span>
+                  <span class="info-box-number">
+                    <a href="{{ route('dokumen-kegiatan.index') }}" class="total-link">{{ $totaldokumenkegiatan }}</a>
+                  </span>
+                </div>
+                <!-- /.info-box-content -->
+              </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+
+            <!-- /.col -->
+            <div class="col-12 col-sm-6 col-md-6">
+              <div class="info-box">
+                <span class="info-box-icon text-bg-warning shadow-sm">
+                  <i class="bi bi-people-fill"></i>
+                </span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Anggota Aktif</span>
+                  <span class="info-box-number">
+                    <a href="{{ route('member.index') }}" class="total-link">{{ $totalmemberaktif }}</a>
+                  </span>
+                  </span>
+                </div>
+                <!-- /.info-box-content -->
+              </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+
+            <!-- /.col -->
+            <div class="col-12 col-sm-6 col-md-6">
+              <div class="info-box">
+                <span class="info-box-icon text-bg-warning shadow-sm">
+                  <i class="bi bi-people-fill"></i>
+                </span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Anggota Tidak Aktif</span>
+                  <span class="info-box-number">
+                    <a href="{{ route('member.index') }}" class="total-link">{{ $totalmembernonaktif }}</a>
+                  </span>
+                </div>
+                <!-- /.info-box-content -->
+              </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+
 
           </div>
           <!-- /.row -->
@@ -101,7 +156,9 @@
                     </button>
                   </div>
                 </div>
-                <div class="card-body" id="grafik"></div>
+                <div class="card-body">
+                  <div id="grafik" style="width:100%; height:400px;"></div>
+                </div>
                 <!-- /.card-body -->
               </div>
               <!-- /.card -->
@@ -115,42 +172,59 @@
     <!--end::App Main-->
     <script src="https://code.highcharts.com/12.4.0/highcharts.js"></script>
     <script type="text/javascript">
-      var grafikKasMasuk = <?php echo json_encode($grafikKasMasuk) ?>;
-      var grafikKasKeluar = <?php echo json_encode($grafikKasKeluar) ?>;
-      var bulan = <?php echo json_encode($bulan) ?>;
+      document.addEventListener('DOMContentLoaded', function() {
+        var grafiktotalsuratmasuk = <?php echo json_encode($grafiktotalsuratmasuk) ?>;
+        var grafiktotalsuratkeluar = <?php echo json_encode($grafiktotalsuratkeluar) ?>;
+        var grafiktotaldokumenkegiatan = <?php echo json_encode($grafiktotaldokumenkegiatan) ?>;
+        var grafiktotalsertifikat = <?php echo json_encode($grafiktotalsertifikat) ?>;
+        var bulan = <?php echo json_encode($bulan) ?>;
 
-      Highcharts.chart('grafik', {
-        title: null,
-        xAxis: {
-          categories: bulan
-        },
-        yAxis: {
-          title: {
-            text: 'Jumlah Data'
+        Highcharts.chart('grafik', {
+          title: null,
+          xAxis: {
+            categories: bulan
           },
-          plotOptions: {
-            series: {
-              allowPointSelect: true
+          yAxis: {
+            title: {
+              text: 'Jumlah Data'
+            },
+            plotOptions: {
+              series: {
+                allowPointSelect: true
+              }
             }
-          }
-        },
-        series: [{
-            name: 'Kas Masuk',
-            data: grafikKasMasuk,
-            color: '#399918'
           },
-          {
-            name: 'Kas Keluar',
-            data: grafikKasKeluar,
-            color: '#980404'
-          }
-        ]
+          series: [{
+              name: 'Surat Masuk',
+              data: grafiktotalsuratmasuk,
+              color: '#399918'
+            },
+            {
+              name: 'Surat Keluar',
+              data: grafiktotalsuratkeluar,
+              color: '#980404'
+            },
+            {
+              name: 'Dokumen Kegiatan',
+              data: grafiktotaldokumenkegiatan,
+              color: '#0D1164'
+            },
+            {
+              name: 'Sertifikat',
+              data: grafiktotalsertifikat,
+              color: '#F87B1B'
+            }
+          ]
+        });
+
       });
     </script>
+
     @include('layout.footer')
   </div>
   <!--end::App Wrapper-->
   @include('layout.script')
+
 </body>
 <!--end::Body-->
 
